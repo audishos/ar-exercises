866bf21281736bf3e146fee56684092b01059b4b
audishos <audisho.sada@gmail.com>
2017-10-10 13:19:24 -0400

    Outputs total revenue, average revenue and # of stores with revenue > M

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,6 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+puts "Total revenue: #{Store.sum(:annual_revenue)}"
+puts "Average revenue: #{Store.sum(:annual_revenue) / Store.count}"
+puts "Stores generating > $1M: #{Store.where(['annual_revenue > ?', 1000000]).size}"

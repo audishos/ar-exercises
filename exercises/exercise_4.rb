f93de6469166ef8286065355dd4e40d377d5a1dd
audishos <audisho.sada@gmail.com>
2017-10-10 13:10:23 -0400

    Adds 3 new stores, finds men's stores, finds women's stores with revenue < 1000000 and outputs them

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,37 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+surrey = Store.new(
+  name: 'Surrey',
+  annual_revenue: 224000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+
+surrey.save
+
+whistler = Store.new(
+  name: 'Whistler',
+  annual_revenue: 1900000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+
+whistler.save
+
+yaletown = Store.new(
+  name: 'Yaletown',
+  annual_revenue: 430000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+yaletown.save
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.map {|store| puts "Store: #{store.name}, Revenue: #{store.annual_revenue}"}
+
+@womens_stores = Store.where(['mens_apparel = ? and annual_revenue < ?', true, 1000000])
+
+@womens_stores.map {|store| puts "Store: #{store.name}, Revenue: #{store.annual_revenue}"}
